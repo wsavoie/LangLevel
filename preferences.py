ded4fdeb3711247a59d8d4f425446003e32e7815
wsavoie <wsavoie@gatech.edu>
2020-09-14 15:36:03 -0500

    implemented configparser for preferences

--- preferences.py
+++ preferences.py
@@ -1,64 +1,171 @@
-from manabi import MainWindow as mw
-import configparser as cp
+import configparser
+# from PyQt5.QtWidgets import *
 import importlib
+import preferences
+import os
+     
+
+def write_prefs():
+    par = os.path.split(PREFS_PATH)[0]
+    if not os.path.exists(par):
+            os.makedirs(par)
+    with open(PREFS_PATH, 'w') as configfile:
+        prefs.write(configfile)
+
+def set_default_prefs():
+    prefs.read_dict(default_settings)
+    write_prefs()
+    
+def add_comments(section,key):
+    prefs[section][key]= prefs[section][key] + '#' + comments[key]
+    # for list to loop through and add comments to the database file for readability
+    # make sure to add # before matching values in the 
+def init():
+    prefs = configparser.ConfigParser()
+    prefs.read(PREFS_PATH)
+    if(len(prefs)==1): #if it is blank
+        set_default_prefs()
+    return prefs
+PREFS_FOLDER='prefs'
+PREFS_FILE='prefs.cfg'
+PREFS_PATH= os.path.join(PREFS_FOLDER,PREFS_FILE)
 
 
-cfg = cp.ConfigParser()
-cfg.read('prefs.cfg')
-# def init_preferences():
-#     _init_config_py()
-
-# def _init_config_py():
-#     global config_py
-#     from . import config
-#     importlib.reload(config)
-#     config_py = config
-def default_prefs():
-    cfg['OPTIONS'] = {'ServerAliveInterval': '45',
-                     'Compression': 'yes',
-                     'CompressionLevel': '9'}
-    cfg['LOCATIONS'] = {r'FrequencyList': r'C:/Users/WS/AppData/Roaming/Anki2/main/dbs/frequency.txt',
-                    'KnownMorphsDir': r'C:\Users\WS\AppData\Roaming\Anki2\main\dbs\known.db'
-                    'Outputdir':r'C:\Users\WS\AppData\Roaming\Anki2\main\dbs'
-                    'morphemizerMainFold': r'C:\Users\WS\PyProjs\Manabi\MM\morph\deps',
-                    }
-
-def _get_config_py_preference(key, modelId=None, deckId=None):
-    assert config_py, 'Tried to use cfgMods before profile loaded'
-    profile = mw.pm.name
-    model = mw.col.models.get(modelId)['name'] if modelId else None
-    deck = mw.col.decks.get(deckId)['name'] if deckId else None
-    if key in config_py.deck_overrides.get(deck, []):
-        return config_py.deck_overrides[deck][key]
-    elif key in config_py.model_overrides.get(model, []):
-        return config_py.model_overrides[model][key]
-    elif key in config_py.profile_overrides.get(profile, []):
-        return config_py.profile_overrides[profile][key]
-    else:
-        return config_py.default[key]
-
-def set_pref(input):
-    cfg
-
-def get_preferences():
-#TODO do this method
-    pref= {
+comments= {
+        'projectfold' : 'programs directory',
+        'dbdir' : 'location where databases reside',
+        'freqencylist': 'location of the frequency list file',
+        'outputdir' : 'location to output stat files to',
+        'morphemizerfold': 'location where morphemizers are found',
+        'knownmorphs': 'location of Known morphs db file',
+        'externalmorphs': 'location of external morphs file',
+        'currmophemizer' : 'name of currently selected morphemizer'
+}
         
+default_settings = {
+    'LOCATIONS'         : {
+    'projectfold'       : os.getcwd(), # should I use relative dir here?:}
+    'dbdir'             : 'C:\\Users\\WS\AppData\\Roaming\\Anki2\\main\\dbs', #should I make a folder to place them in the project?
+    'outputdir'         : '%(projectfold)s\\output',
+    'morphemizerfold'   : '%(projectfold)s\\deps'
+    },
+    
+    'DATABASES'         : {
+    'frequencylist'     : '%(dbdir)s\\frequency.txt',
+    'knownmorphs'       : '%(dbdir)s\\known.db',
+    'externalmorphs'    : '%(dbdir)s\\external.db',
+    'currmophemizer'    : 'mecab'    
+    },
+
+    'VALUES'            : {
+    'min_master_freq'   : '0',
+    'read_target'       : '98'   
+    },
+    
+    'CHECKBOXES'        : {
+    'save_study_plan'   :'1',
+    'save_word_report'  :'1',
+    'save_freqency_list':'1'    
     }
-    return pref
-def init_preferences():
-    # TODO write this method
-    pref = {}
+}
 
-def get_preference(key, model_id=None, deck_id=None):
-    try:
-        return _get_config_py_preference(key, model_id, deck_id)
-    except KeyError:
-        return _get_anki_json_config(key)
+if 'prefs' not in locals():
+    prefs=init()
 
-def update_preferences(jcfg):
-    pref_copy = mw.col.conf['addons']['morphman'].copy()
-    pref_copy.update(jcfg)
-    if not mw.col.conf['addons']['morphman'] == pref_copy:
-        mw.col.conf['addons']['morphman'] = pref_copy
-        mw.col.setMod()
+   
+
+   
+
+
+# for key in config['LOCATIONS']:
+#     add_comments('LOCATIONS')
+# print(prefs['LOCATIONS']['frequencylist'])
+
+
+# cfg['OPTIONS'] = {'ServerAliveInterval': '45',
+#                      'Compression': 'yes',
+#                      'CompressionLevel': '9'}
+# cfg['LOCATIONS'] = {r'FrequencyList': r'C:/Users/WS/AppData/Roaming/Anki2/main/dbs/frequency.txt',
+#                     'KnownMorphsDir': r'C:\Users\WS\AppData\Roaming\Anki2\main\dbs\known.db',
+#                     'Outputdir': r'C:\Users\WS\AppData\Roaming\Anki2\main\dbs',
+#                     'morphemizerMainFold': r'C:\Users\WS\PyProjs\Manabi\MM\morph\deps',
+#                     }
+#  with open('')       
+
+
+
+# class manabuPrefs(configparser.ConfigParser):
+#     def __init__(self):
+#         super().__init__()
+#         self.PREFS_FOLDER='prefs'
+#         self.PREFS_FILE='prefs.cfg'
+#         self.PREFS_PATH= os.path.join(self.PREFS_FOLDER,self.PREFS_FILE)
+        
+#         # TODO if empty perform set defaults 
+
+
+#         self.comments= {
+#         'projectfold' : 'programs directory',
+#         'dbdir' : 'location where databases reside',
+#         'freqencylist': 'location of the frequency list file',
+#         'outputdir' : 'location to output stat files to',
+#         'morphemizerfold': 'location where morphemizers are found',
+#         'knownmorphs': 'location of Known morphs db file',
+#         'externalmorphs': 'location of external morphs file',
+#         'currmophemizer' : 'name of currently selected morphemizer'
+#         }
+        
+#         self.default_settings = {
+#             'LOCATIONS'         : {
+#             'projectfold'       : os.getcwd(), # should I use relative dir here?:}
+#             'dbdir'             : 'C:\\Users\\WS\AppData\\Roaming\\Anki2\\main\\dbs', #should I make a folder to place them in the project?
+#             'outputdir'         : '%(projectfold)s\\output',
+#             'morphemizerfold'   : '%(projectfold)s\\deps'
+#             },
+            
+#             'DATABASES'         : {
+#             'frequencylist'     : '%(dbdir)s\\frequency.txt',
+#             'knownmorphs'       : '%(dbdir)s\\known.db',
+#             'externalmorphs'    : '%(dbdir)s\\external.db',
+#             'currmophemizer'    : 'mecab'    
+#             },
+
+#             'VALUES'            : {
+#             'min_master_freq'   : '0',
+#             'read_target'       : '98'   
+#             },
+            
+#             'CHECKBOXES'        : {
+#             'save_study_plan'   :'1',
+#             'save_word_report'  :'1',
+#             'save_freqency_list':'1'    
+#             }
+#         }
+
+#         self.read(self.PREFS_PATH)
+#         if(len(self)==1):
+#             self.read_dict(self.default_settings)
+#             self.write_prefs()
+
+#     def write_prefs(self):
+#         par = os.path.split(self.PREFS_PATH)[0]
+#         if not os.path.exists(par):
+#                 os.makedirs(par)
+#         with open(self.PREFS_PATH, 'w') as configfile:
+#             self.write(configfile)
+
+#     def set_default_prefs(self):
+#         #TODO change these before deploying
+#         pass
+#         # write_prefs()
+#     def add_comments(self, section,key):
+#         prefs[section][key]= prefs[section][key] + '#' + self.comments[key]
+#         # for list to loop through and add comments to the database file for readability
+#         # make sure to add # before matching values in the 
+
+#     def load_prefs(self):
+#         self.read(self.PREFS_PATH)
+
+# if __name__ == "__main__":
+#     prefs=manabuPrefs()
+   
--- testCode.py
+++ testCode.py
@@ -1,15 +1,4 @@
-import configparser
+from preferences import *
 
-config = configparser.ConfigParser()
-config['DEFAULT'] = {'ServerAliveInterval': '45',
-                     'Compression': 'yes',
-                     'CompressionLevel': '9'}
-config['bitbucket.org'] = {}
-config['bitbucket.org']['User'] = 'hg'
-config['topsecret.server.com'] = {}
-topsecret = config['topsecret.server.com']
-topsecret['Port'] = '50022'     # mutates the parser
-topsecret['ForwardX11'] = 'no'  # same here
-config['DEFAULT']['ForwardX11'] = 'yes'
-with open('config.ini', 'w') as configfile:
-    config.write(configfile)
+print(prefs['VALUES']['read_target'])
+prefs['VALUES']['read_target']='91'
