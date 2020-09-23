4f1f42db8cb3fff485127f11097b7c0d846b13e5
wsavoie <wsavoie@gatech.edu>
2020-09-23 12:37:57 -0500

    manabi class includes new windows

--- manabi.py
+++ manabi.py
@@ -3,114 +3,167 @@ import sys
 # from PyQt5.QtGui import *
 # from PyQt5.QtWidgets import *
 
-from PyQt5 import QtWidgets, QtGui, QtCore, uic
-from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel, QGridLayout, QWidget, QCheckBox, QSystemTrayIcon, \
-    QSpacerItem, QSizePolicy, QMenu, QAction, QStyle, qApp, QMessageBox
+from PyQt5 import QtWidgets, QtGui, QtCore, uic, Qt
+from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel, QGridLayout, QWidget, QCheckBox, QSystemTrayIcon,\
+     QSpacerItem, QSizePolicy, QMenu, QAction, QStyle, qApp, QMessageBox
 from PyQt5.QtCore import QSize
 
-import manabi_ui
+from preferences import p
 import importlib
-
-
+from UI import manabi_ui, morphemizerComboBox, aboutWindow, propertiesWindow, database_manager
+import util
+import platform
 
 #pyuic5 -x manabi.ui -o manabi_ui.py
 
 
 class MainWindowUI(manabi_ui.Ui_MainWindow):
-    def __init__( self ):
+    def __init__( self,mainwindow):
         '''Initialize the super class
         '''
         super().__init__()
+        self.mw = mainwindow
 
         # QMainWindow.__init__(self)
-        
-
     def errorMsg(self, text):
         msg = QMessageBox()
         msg.setWindowTitle("error")
         msg.setText(text)
         msg.exec_()
-        
 
-    def setupUi(self,MainWindow):
+    def setupUi(self):
         ''' Setup the UI of the super class, and add here code
         that relates to the way we want our UI to operate.
         '''
-        super().setupUi(MainWindow)
+        super().setupUi(self.mw)
         # Init QSystemTrayIcon
         manabi_icon = QtGui.QIcon()
-        manabi_icon.addFile('test.png', QtCore.QSize(20,20))
-        self.tray_icon = QSystemTrayIcon()
+        manabi_icon.addFile('icons//test.png', QtCore.QSize(20,20))
+        self.tray_icon = QSystemTrayIcon(self.mw)
         self.tray_icon.setIcon(manabi_icon)
+        
+        # self.tray_icon.setIcon(self.mw.style().standardIcon(QStyle.SP_ComputerIcon))
         # self.
         # self.actionExit = self.findChild(QtWidgets.QAction, "actionExit")
-
-
+        # app.setQuitLockEnabled(False)
+        #TODO maybe use qApp to find a closeEven thing
+        app.aboutToQuit.connect(self.closeEvent)
+        # self.mw.isminimized.connect(self.)
+        # self.mw.showMinimized()
         '''
             Define and add steps to work with the system tray icon
             show - show window
             hide - hide window
             exit - exit from application
-        '''
-        show_action = QAction("Show",MainWindow)
-        quit_action = QAction("Exit",MainWindow)
-        hide_action = QAction("Hide",MainWindow)
-        show_action.triggered.connect(MainWindow.show)
-        hide_action.triggered.connect(MainWindow.hide)
-        # quit_action.triggered.connect(qApp.quit)
-        # quit_action.triggered.connect(self.closeEvent)
-        quit_action.triggered.connect(MainWindow.close)
-       
-        # show_action = QAction("Show",self)
-        # quit_action = QAction("Exit",self)
-        # hide_action = QAction("Hide",self)
-        # show_action.triggered.connect(self.show)
-        # hide_action.triggered.connect(self.hide)
-        # quit_action.triggered.connect(qApp.quit)
-        tray_menu = QMenu()
+        '''    
+        show_action = QAction("Show",self.mw)
+        hide_action = QAction("Minimize to tray",self.mw)
+        quit_action = QAction("Exit",self.mw)
+        show_action.triggered.connect(self.mw.show)
+        hide_action.triggered.connect(self.mw.hide)
+        quit_action.triggered.connect(qApp.quit)
+        tray_menu = QMenu(self.mw)
         tray_menu.addAction(show_action)
         tray_menu.addAction(hide_action)
+        tray_menu.addSeparator()
         tray_menu.addAction(quit_action)
         self.tray_icon.setContextMenu(tray_menu)
         self.tray_icon.show()
- 
-    def closeEvent(self,event):
         
-        print('closeEvent')
-        
-        if self.minimize_to_tray.isChecked():
-            
-            event.ignore()
-            MainWindow.hide()
-            MainWindow.tray_icon.showMessage(
-                "Manabi",
-                "Application was minimized to Tray",
-                QSystemTrayIcon.Information,
-                2000
-            )     
+        '''
+            initialize all values to values in property file
 
+        '''
+        
+        self.minimize_to_tray.setChecked(p['DEFAULT'].getboolean('minimizetotray'))
+
+
+        '''
+            set actions 
+        '''
+        self.minimize_to_tray.clicked['bool'].connect(lambda: util.on_check('minimizetotray'))
+   
+######################
+
+        '''
+            setup menus items
+        '''
+        self.actionProperties_2.triggered.connect(self.onProperties)
+        self.actionAbout.triggered.connect(self.onAbout)
+        self.actionDatabase_Manager.triggered.connect(self.onDatabase_Manager)
+          
+    def onDatabase_Manager(self):
+        database_manager.main(self.mw)
+    def onProperties(self):
+        propertiesWindow.main(self.mw)
+
+    def onAbout(self):
+        dialog = QtWidgets.QDialog(self.mw)
+        about = aboutWindow.Ui_Dialog()
+        about.setupUi(dialog)
+        ver= 1.0
+        about.versioningInfo.setText(f"Manabi version: {str(ver)} \nPython: {platform.python_version()}\nPyQt: {vars(Qt)['PYQT_VERSION_STR']}")
+        about.manabi_pic.setPixmap(QtGui.QPixmap('icons//manabi.png'))
+        dialog.show()
+        
+        # self.
+
+        # c= aboutWindow.aboutDialog(self.mw)
+        # cmain(self.mw)
+       
+        # c.exec_() 
+    
+    def closeEvent(self):
+        p.write_prefs()
+        #TODO add save all preferences to pref file
+        
     # def closeEvent(self):
     #     print('hello from other close')
+    def show_minimized_msg(self):
+        self.tray_icon.showMessage(
+            "Manabi",
+            "Application was minimized to Tray",
+            QSystemTrayIcon.Information,
+            1500)
+    def on_minimize(self):
+        print(self.mw.isMinimized())
 
-    
+        if(self.mw.isMinimized()):
+            if p['DEFAULT'].getboolean('minimize_to_tray'):
+                self.mw.hide()
+
 
 def main():
     # mw.mm = Morphman(mw)
     # mw.mm.show()
-    print('hi')
+    # app = QtWidgets.QApplication(sys.argv)
+    
+
+    MainWindow = QtWidgets.QMainWindow()
+
+    ui = MainWindowUI(MainWindow)
+    ui.setupUi()
+
+    if p['DEFAULT'].getboolean('open_minimized'):
+        ui.mw.hide()
+        ui.show_minimized_msg()
+    else:
+        ui.mw.show()
+    
+    sys.exit(app.exec_())
     
 
 if __name__ == "__main__":
     import sys
     app = QtWidgets.QApplication(sys.argv)
-    MainWindow = QtWidgets.QMainWindow()
-    ui = MainWindowUI()
-    ui.setupUi(MainWindow)
-    MainWindow.show()
+    main()
+#     MainWindow = QtWidgets.QMainWindow()
+#     ui = MainWindowUI(MainWindow)
+#     ui.setupUi()
+#     MainWindow.show()
 
-    ui.errorMsg('err')
     
-    sys.exit(app.exec_())
+    
 
     # # MainWindow = QtWidgets.QMainWindow()
     # app = QtWidgets.QApplication(sys.argv)
@@ -120,7 +173,7 @@ if __name__ == "__main__":
     # sys.exit(app.exec_())
     
 
-main()
+
 
 
 # class Ui_MainWindow(QtWidgets.QMainWindow):
