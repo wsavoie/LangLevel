39e9e579020d57b105183f0cd8225dff870c9b9a
wsavoie <wsavoie@gatech.edu>
2020-09-25 18:17:05 -0500

    database manager to table setup

--- UI/database_manager.py
+++ UI/database_manager.py
@@ -1,14 +1,21 @@
-from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5 import QtCore, QtGui, QtWidgets, Qt
 import sys
-import util
 
+import os
 # from propertiesWindow_ui import Ui_Form
 from .database_manager_ui import Ui_Dialog
+from preferences import p
+
+
 
 #TODO fix this
-
 sys.path.append("..") # Adds higher directory to python modules path.
-# from manabi import MainWindowUI
+from morphemes import MorphDb
+from morphemizer import getAllMorphemizers
+from util import errorMsg, getPathLE,CopySelectedCellsAction
+from UI import morphemizerComboBox 
+#TODO make sure language is set somewhere, add a combobox class 
+
 
 class databaseWindow(Ui_Dialog):
     def __init__(self,parent=None):
@@ -22,11 +29,223 @@ class databaseWindow(Ui_Dialog):
             initialize all values to values in property file
 
         '''
+        # AFileButton     
+        # BFileButton
+        # dbAPath
+        # dbBPath
+        # showA
+        # showB
+        # ADiffB
+        # BDiffA
+        # symmetricDiff
+        # ABUnion
+        # ABIntersection
+        # morphemizer
+        # extractMorphemes
+        # saveResults
+        
+        # onlyWords
+        # fullInfo
 
+        # dbTable
+        # dbInfo
+
+        self.AFileButton.clicked.connect(lambda: [None, getPathLE(self.dbAPath,'location of database file',False,
+            startPath=os.path.normpath(p['DEFAULT']['dbdir']),filter_='database dir (*.db)')][0])
+        self.BFileButton.clicked.connect(lambda: [None, getPathLE(self.dbBPath,'location of database file',False,
+            startPath=os.path.normpath(p['DEFAULT']['dbdir']),filter_='database dir (*.db)')][0])
+
+
+        self.showA.clicked.connect(self.onShowA)
+        self.showB.clicked.connect(self.onShowB)
+
+        #comparisons
+        self.ADiffB.clicked.connect(lambda: self.DbInteractions('A-B'))
+        self.BDiffA.clicked.connect(lambda: self.DbInteractions('B-A'))
+        self.symmetricDiff.clicked.connect(lambda: self.DbInteractions('sym'))
+        self.ABUnion.clicked.connect(lambda: self.DbInteractions('union'))
+        self.ABIntersection.clicked.connect(lambda: self.DbInteractions('inter'))
+
+        self.extractMorphemes.clicked.connect(self.onExtractTxtFile)
+        self.saveResults.clicked.connect(self.onSaveResults)
+
+
+        self.db = None
+
+        self.morphemizerComboBox = morphemizerComboBox.MorphemizerComboBox()
+        self.morphemizerComboBox.setMorphemizers(getAllMorphemizers())
+        
+        self.fullInfo.clicked.connect(self.colModeButtonListener)
+        self.onlyMorphs.clicked.connect(self.colModeButtonListener)
         '''
             set actions 
         '''
-        # self.minimize_to_tray.clicked['bool'].connect(lambda: util.on_check('minimizetotray'))
+        self.dbTable.addAction(CopySelectedCellsAction(self.dbTable))
+        # util.CopySelectedCellsAction()
+    def onSaveResults(self):
+        pass
+        #TODO add method
+
+    #     dir_path = cfg('path_dbs') + os.sep + 'results.db'
+    #     destPath = QFileDialog.getSaveFileName(caption='Save results to?', directory=dir_path)[0]
+    #     if not destPath:
+    #         return
+    #     if not hasattr(self, 'db'):
+    #         return errorMsg('No results to save')
+    #     self.db.save(str(destPath))
+    #     infoMsg('Saved successfully')
+    def onExtractTxtFile(self):
+        pass
+        #TODO add method
+        #TODO add open on drag
+    
+        # srcPath = QFileDialog.getOpenFileName(caption='Text file to extract from?', directory=cfg('path_dbs'))[0]
+        # if not srcPath:
+        #     return
+
+        # destPath = QFileDialog.getSaveFileName(
+        #            caption='Save morpheme db to?', directory=cfg('path_dbs') + os.sep + 'textFile.db')[0]
+        # if not destPath:
+        #     return
+
+        # mat = cfg('text file import maturity')
+        # db = MorphDb.mkFromFile(str(srcPath), self.morphemizerComboBox.getCurrent(), mat)
+        # if db:
+        #     db.save(str(destPath))
+        #     infoMsg('Extracted successfully')
+    def DbInteractions(self, kind):
+        try:
+            self.loadAB()
+        except Exception as e:
+            return errorMsg('Can\'t load dbs:\n%s' % e)
+
+        a_set = set(self.aDb.db.keys())
+        b_set = set(self.bDb.db.keys())
+        if kind == 'sym':
+            ms = a_set.symmetric_difference(b_set)
+        elif kind == 'A-B':
+            ms = a_set.difference(b_set)
+        elif kind == 'B-A':
+            ms = b_set.difference(a_set)
+        elif kind == 'inter':
+            ms = a_set.intersection(b_set)
+        elif kind == 'union':
+            ms = a_set.union(b_set)
+        else:
+            raise ValueError("'kind' must be one of [sym, A-B, B-A, inter, union], it was actually '%s'" % kind)
+
+        self.db.clear()
+        for m in ms:
+            locs = set()
+            if m in self.aDb.db:
+                locs.update(self.aDb.db[m])
+            if m in self.bDb.db:
+                locs.update(self.bDb.db[m])
+            self.db.addMLs1(m, locs)
+
+        self.updateDisplay()
+
+    def loadA(self):
+        self.aPath = self.dbAPath.text()
+        self.aDb = MorphDb(path=self.aPath)
+        if not self.db:
+            self.db = self.aDb
+
+    def loadB(self):
+        self.bPath = self.dbBPath.text()
+        self.bDb = MorphDb(path=self.bPath)
+
+    def loadAB(self):
+        self.loadA()
+        self.loadB()
+
+    def onShowA(self):
+        try:
+            self.loadA()
+        except Exception as e:
+            return errorMsg(f"Cannot load db:\n {e}")
+        self.db = self.aDb
+        self.updateDisplay()
+
+    def onShowB(self):
+        try:
+            self.loadB()
+        except Exception as e:
+            return errorMsg(f"Cannot load db:\n {e}")
+        self.db = self.bDb
+        self.updateDisplay()
+
+    def colModeButtonListener(self):
+        # colModeButton = self.sender()
+        # if colModeButton.isChecked():
+        try:
+            self.updateDisplay()
+        except AttributeError:
+            return  # User has not selected a db view yet
+
+    def updateDisplay(self):
+        # # num_cols = get from a morpehme component
+        # num_cols = 6
+        # cols=['']
+        # if num_cols == 6: #mecab
+        #     #name columns
+        #     cols=['norm base', 'base', 'inflected', 'reading', 'POS','subPOS', 'freq']
+        # else:
+        #     cols=[f"{i}" for i in range(7)]
+        self.dbTable.clear()
+        self.posTable.clear()
+        self.dbTable.setRowCount(len(self.db.db.items()))
+        if self.fullInfo.isChecked():
+
+            # self.dbTable.setText(self.db.showMs())
+            #  return sorted(self.db.items(), key=lambda it: it[0].show()))
+            self.dbTable.setColumnCount(7)
+            self.dbTable.setHorizontalHeaderLabels(['norm base', 'base', 'inflected', 'reading', 'POS','subPOS', 'freq'])
+            # errorMsg('yo')
+            for i,m in enumerate(self.db.showMsList()):
+                # freq= len(m[1])
+                self.addRow(self.dbTable,i,[m[0].norm, m[0].base, m[0].inflected, m[0].read, m[0].pos, m[0].subPos,str(len(m[1]))])            
+    
+        else:
+            self.dbTable.setColumnCount(1)
+            self.dbTable.setHorizontalHeaderLabels(['morphs'])
+            for i,m in enumerate(self.db.showMsList()):
+                self.addRow(self.dbTable,i,[m[0].norm]) 
+            # self.dbTable.setText('\n'.join(sorted(list(set([m.norm for m in self.db.db])))))
+        self.db.analyze()
+
+        
+        self.summaryLabel.setText(f'Total normalized morphemes: {self.db.kCount}\nTotal variations: {self.db.vCount}\nBy part of speech:')
+ 
+        self.posTable.setColumnCount(3)
+        self.posTable.setHorizontalHeaderLabels(['Total',r'% of Total','POS'])
+        self.posTable.setRowCount(len(self.db.posBreakdown.items()))  
+        for i,k in enumerate(self.db.posBreakdown.items()):
+            self.addRow(self.posTable,i,[str(k[1]), str(int(100. * k[1] / self.db.vCount)),k[0]]) 
+    def addRow(self,table_,rowInd,vals):
+        for i in range(len(vals)):
+            table_.setItem(rowInd,i,QtWidgets.QTableWidgetItem(vals[i]))
+def getProgressWidget():
+    progressWidget = QtWidgets.QWidget()
+    progressWidget.setFixedSize(400, 70)
+    progressWidget.setWindowModality(Qt.WindowModal)
+    bar = QtWidgets.QProgressBar(progressWidget)
+
+    #TODO add utils function to determine OS
+    isMac = False
+    #
+    if isMac:
+        bar.setFixedSize(380, 50)
+    else:
+        bar.setFixedSize(390, 50)
+    bar.move(10, 10)
+    per = QtWidgets.QLabel(bar)
+    per.setAlignment(Qt.AlignCenter)
+    progressWidget.show()
+    return progressWidget, bar
+
+
+
 if __name__ == "__main__":
     import sys
     app = QtWidgets.QApplication(sys.argv)
--- util.py
+++ util.py
@@ -1,8 +1,108 @@
-from manabi import mw
-from .preferences import *
 
-def mkBtn(txt, f, parent):
-    b = QPushButton(txt)
-    b.clicked.connect(f)
-    parent.addWidget(b)
-    return b
+from preferences import p
+import os
+from PyQt5 import QtCore, QtGui, QtWidgets
+import platform
+
+import hashlib, binascii
+
+class CopySelectedCellsAction(QtWidgets.QAction):
+    def __init__(self, table_widget):
+        if not isinstance(table_widget, QtWidgets.QTableWidget):
+            raise ValueError(str('CopySelectedCellsAction must be initialised with a QTableWidget. A %s was given.' % type(table_widget)))
+        super(CopySelectedCellsAction, self).__init__("Copy", table_widget)
+        self.setShortcut('Ctrl+C')
+        self.triggered.connect(self.copy_cells_to_clipboard)
+        self.table_widget = table_widget
+
+    def copy_cells_to_clipboard(self):
+        if len(self.table_widget.selectionModel().selectedIndexes()) > 0:
+            # sort select indexes into rows and columns
+            RC=self.table_widget.selectedRanges()[0].rowCount()
+            CC=self.table_widget.selectedRanges()[0].columnCount()
+            items=self.table_widget.selectedItems()
+            
+            #if selection leads to a ragged array, only copy first value
+            if(len(items) is not RC*CC): 
+                sys_clip = QtWidgets.QApplication.clipboard()
+                sys_clip.setText(items[0].text())
+            
+            outList=[[items[i].text() for i in range(j*CC,(j+1)*CC)] for j in range(RC)]
+            outCommand= '\n'.join([x for x in map('\t'.join,outList)])
+            sys_clip = QtWidgets.QApplication.clipboard()
+            sys_clip.setText(outCommand)
+
+
+def errorMsg(text):
+    msg = QtWidgets.QMessageBox()
+    msg.setWindowTitle("error")
+
+    msg.setText(text)
+    msg.exec_()
+
+def isWin():
+    if platform.system()=='Windows':
+        return True
+    return False
+
+def isMac():
+    if platform.system()=='Darwin':
+        return True
+    return False
+
+def isLinux():
+    if platform.system()=='Linux ':
+        return True
+    return False
+
+def getPathLE(lineEdit, caption, open_directory=False,startPath='',filter_=''):  # LineEdit -> GUI ()
+    '''get database files from explorer add them to lineEdit'''
+    try:
+        if open_directory:
+            path =  QtWidgets.QFileDialog.getExistingDirectory(caption=caption, directory=startPath,
+                        options=QtWidgets.QFileDialog.ShowDirsOnly)
+        else:
+            path = QtWidgets.QFileDialog.getOpenFileName(caption=caption, directory=startPath, filter=filter_)[0]
+    except Exception as e:
+        errorMsg(e)
+        return
+
+    if path:
+        lineEdit.setText(path)
+
+def getPathLW(listWid,caption, open_directory=False,startPath='',filter_=''):  # LineEdit -> GUI ()
+    '''get database files from explorer for use when adding to line widget'''
+    try:
+        paths = QtWidgets.QFileDialog.getOpenFileNames(caption=caption, directory=startPath, filter=filter_)[0]
+        # print(paths)
+    except Exception as e:
+        print(e)
+        return
+    
+    return paths
+
+def setCheck(obj,prefName):
+    obj.setChecked(p['DEFAULT'].getboolean(prefName))
+    obj.clicked['bool'].connect(lambda: on_check(prefName))
+    
+def on_check(prop):
+    p['DEFAULT'][prop]= str(not p['DEFAULT'].getboolean(prop))
+
+def retrive_pass(stored_password, provided_password):
+    """Verify a stored password against one provided by user"""
+    salt = stored_password[:64]
+    stored_password = stored_password[64:]
+    pwdhash = hashlib.pbkdf2_hmac('sha512', 
+                                  provided_password.encode('utf-8'), 
+                                  salt.encode('ascii'), 
+                                  100000)
+    # pwdhash = binascii.hexlify(pwdhash).decode('ascii')
+    return binascii.hexlify(pwdhash).decode('ascii')
+
+def hash_password(password):
+    """Hash a password for storing."""
+    salt = hashlib.sha256(os.urandom(60)).hexdigest().encode('ascii')
+    pwdhash = hashlib.pbkdf2_hmac('sha512', password.encode('utf-8'), 
+                                salt, 100000)
+    pwdhash = binascii.hexlify(pwdhash)
+    return (salt + pwdhash).decode('ascii')
