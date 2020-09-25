b6da2de2d95341b9c74461a3b75166fce80a97d8
wsavoie <wsavoie@gatech.edu>
2020-09-25 18:20:13 -0500

    put wanikani code into import data

--- importData.py
+++ importData.py
@@ -1,28 +1,52 @@
-
-
-import requests
 import json
-import pandas as pd
+
 import numpy as np
+import pandas as pd
+import requests
+
 from preferences import p
 
-WKAUTHHEAD={'Authorization': f'Bearer {p["wk_api_key"]}'}
+#https://docs.python.org/2.5/whatsnew/pep-343.html
+#write a wanikani context manager?
+WKAUTHHEAD={f'Authorization' : f'Bearer {p["DEFAULT"]["wk_api_key"]}'}
 WK_URL=r"https://api.wanikani.com/v2/{0}"
 
+
+def processWKResponse(response):
+    # 200 	Success 	n/a
+    # 401 	Unauthorized 	“Unauthorized. Nice try.”
+    # 403 	Forbidden 	
+    # 404 	Not Found 	
+    # 422 	Unprocessable Entity 	Description of how the request was malformed.
+    # 429 	Too Many Requests 	
+    # 500 	Internal Server Error 	n/a
+    # 503 	Service Unavailable 	n/a
+    
+    # https://stackoverflow.com/questions/2052390/manually-raising-throwing-an-exception-in-python
+    rc=response.status()
+    rc=200
+    if rc is 200:
+        return response
+    elif rc == 401: #key error
+        return response.raise_for_status()
+    return
 
 def getWKLevel():
     
     response = requests.request("GET", WK_URL.format('user'), headers={**WKAUTHHEAD} , data = {})
-    print('sc=',response.status_code)
-    rc = response.status_code
+    try:
+        response.raise_for_status()
+    except Exception as e:
+        print(e)
+        return
     wklevel=json.loads(response.text)['data']['level']
-    if(rc is not 200): #200 is correct code,  will return 401 is problematic APIKEY
-        pass
-        #TODO error action here
     return wklevel
 
 def getWKData(wklevel):
-    
+    #TODO implement caching 
+    #  conditional requests, which lets us quickly tell you that a record hasn't changed since you got it last.
+    #  The second is to give you tools to get only the updated or new records after any point in time,
+    #  letting you easily refresh your local data caches and stores without having to parse all the records. 
     #create string of 1 to wanikani level
     lvlString=str([i for i in range(1,wklevel+1)]).strip('[]').replace(' ', '')
     wkData= pd.DataFrame()
@@ -40,14 +64,14 @@ def getWKData(wklevel):
             )
         )
         nexturl=resp['pages']['next_url']
-    wkData.set_index(0,inplace=True)
+    # wkData.set_index(0,inplace=True)
+    # print(wkData)
+    # wkData.to_csv(DBDIR.format('wanikaniVocab.txt'))
     return wkData   
-
+    
 # ********************************
 # **** duolingo stuff
 # ********************************
 def importFromDuolingo():
     pass
     #TODO import words from duolingo
-
-
